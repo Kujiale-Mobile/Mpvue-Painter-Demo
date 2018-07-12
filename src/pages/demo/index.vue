15d72fbc706f360bdd6d3b3f4b7b954ec8952e06
csldev <335012480@qq.com>
2018-07-12 17:34:02 +0800

    demo所需页面

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button @click="save">保存</button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    save () {
+      console.log('on save click')
+    }
+  }
+}
+</script>
