e881111b94982189c621defd78df2db56d0c843e
dongqing <dongqing@qunhemail.com>
2018-07-26 15:51:22 +0800

    update painter

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -18,6 +18,7 @@ const customStyle = 'margin-left:40rpx'
 export default {
   data () {
     return {
+      shareImg: '',
       customStyle: customStyle,
       template: template
     }
@@ -27,6 +28,7 @@ export default {
       console.log('on save click')
     },
     onImgOk (e) {
+      this.shareImg = e.mp.detail.path
       // 两种路径是一样的
       console.log(e.mp.detail.path)
       console.log(e.target.path)
